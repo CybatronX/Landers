8d07f6f56142be6c3ae00a465b75d7c32955bc19
Ajay Anandan <ajayan@microsoft.com>
2016-09-26 18:33:54 -0700

    add scripts

---
+++ ##TemplateScripts/compressed/Dynamic-Call-Out/index.php
@@ -0,0 +1,11 @@
+<html>
+<head>
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/2.1.4/jquery.min.js"></script>
+</head>
+<body>
+
+
+	<script><?php echo($_GET["keyword"]) ?></script>
+
+</body>
+</html>
